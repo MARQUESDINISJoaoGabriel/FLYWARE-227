b63558ab33d237fc31c7edb7c24cdbf9759f6650
MARQUESDINISJoaoGabriel <joao-gabriel.marques-dinis@efrei.net>
2024-12-15 00:13:18 +0100

    Structure V1

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/src/Router.php';
-use \src\Router;
+
+
+use src\controller;
+use src\Router;
+
 
 $router = new Router();
 
 $router->addRoute('/FLYWARE-227', 'RadioController@list');
-$router->addRoute('/FLYWARE-227/radio', 'RadioController@list');
 $router->addRoute('/FLYWARE-227/radio/message', 'RadioController@message');
 $router->addRoute('/FLYWARE-227/radio/interface', 'RadioController@interface');
 
